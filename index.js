e0332f3f916e7903ce349afae0f6c057fc92ada9
Daniil <palidos@yandex.ru>
2019-12-08 20:20:17 +0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// const funct = document.getElementById('function');
+const points = document.getElementById('points');
 const calc = document.getElementById('calc');
 const startRange = document.getElementById('startRange');
 const endRange = document.getElementById('endRange');
@@ -6,17 +6,19 @@ const res = document.getElementById('res');
 
 const sin = Math.sin;
 const cos = Math.cos;
+const ln = Math.log;
 
 calc.addEventListener('click', () => {
 	// let func = funct.value ? evalee : x => ((x ** 3 + sin(x / 4)) / (x ** 2 + x + 10));
-	let func = x => ((x ** 2 + cos(x / 7)) / (x ** 5 + x + 25));
-	let intervalBegin = startRange.value ? +startRange.value : 0;
-	let intervalEnd = endRange.value ? +endRange.value : 1;
-	let iterations = 1000000;
-	let step = (intervalEnd - intervalBegin) / iterations
-	let integral = 0
-	for (let i = 0; i < iterations; i++) {
-		integral += step * func(intervalBegin + (i - 1) * step);
+	// let func = x => ((x ** 2 + cos(x / 7)) / (x ** 5 + x + 25));
+	let func = x => (1 / ln(x));
+	let intervalBegin = startRange.value ? +startRange.value : 2;
+	let intervalEnd = endRange.value ? +endRange.value : 5;
+	let iterations = points.value ? +points.value : 100000;
+	let step = (intervalEnd - intervalBegin) / iterations;
+	let integral = 0;
+	for (let i = 1; i <= iterations; i++) {
+		integral += step * func(intervalBegin + (i - 1) * step)
 	}
 	res.innerHTML = `Integral is equal to: ${integral}`;
 	console.log(`Integral is equal to: ${integral}`);
